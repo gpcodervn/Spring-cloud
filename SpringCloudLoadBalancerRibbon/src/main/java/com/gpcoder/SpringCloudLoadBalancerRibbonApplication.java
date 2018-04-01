8525aac3cbcc653ceb604933248310f4827406c9
Giang Phan <ptgiang56@gmail.com>
2018-04-01 18:30:56 +0700

    Tìm hiểu về cân bằng tải trong Spring Cloud với Ribbon

---
+++ SpringCloudLoadBalancerRibbon/src/main/java/com/gpcoder/SpringCloudLoadBalancerRibbonApplication.java
@@ -0,0 +1,16 @@
+package com.gpcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@RibbonClient(name = "ping-a-server", configuration = RibbonConfiguration.class)
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCloudLoadBalancerRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLoadBalancerRibbonApplication.class, args);
+	}
+}
