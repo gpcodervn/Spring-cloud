6274ddbfe1f8776e0228930a4d187324d98b435c
Giang Phan <ptgiang56@gmail.com>
2018-04-01 17:58:11 +0700

    Tìm hiểu về Spring Cloud Discovery Eureka Client

---
+++ SpringCloudDiscoveryEurekaClient/bat/abc-service-replica05.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=abc-service-replica05 SpringCloudDiscoveryEurekaClient-0.0.1-SNAPSHOT.jar
---
+++ SpringCloudDiscoveryEurekaClient/src/main/java/com/gpcoder/SpringCloudDiscoveryEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.gpcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCloudDiscoveryEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDiscoveryEurekaClientApplication.class, args);
+	}
+}
