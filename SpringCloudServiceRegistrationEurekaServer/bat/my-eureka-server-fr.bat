0e2e0beafd7d811789b109d75e7cd359938ddbcb
Giang Phan <ptgiang56@gmail.com>
2018-04-01 17:36:51 +0700

    Tìm hiểu về Spring Cloud Eureka Server

---
+++ SpringCloudServiceRegistrationEurekaServer/bat/my-eureka-server-fr.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=france SpringCloudServiceRegistrationEurekaServer-0.0.1-SNAPSHOT.jar
---
+++ SpringCloudServiceRegistrationEurekaServer/src/main/java/com/gpcoder/SpringCloudServiceRegistrationEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.gpcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudServiceRegistrationEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServiceRegistrationEurekaServerApplication.class, args);
+	}
+}
