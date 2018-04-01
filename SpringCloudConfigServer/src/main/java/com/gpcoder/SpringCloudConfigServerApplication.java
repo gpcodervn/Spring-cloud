1343140747676689bb4f089632c2809d95b401e8
Giang Phan <ptgiang56@gmail.com>
2018-04-01 14:01:16 +0700

    Update config server

--- SpringCloudConfigServer/src/main/java/com/gpcoder/SpringCloudConfigServerApplication.java
+++ SpringCloudConfigServer/src/main/java/com/gpcoder/SpringCloudConfigServerApplication.java
@@ -2,7 +2,13 @@ package com.gpcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Start server and run at url:
+ * http://localhost:8888/spring-cloud-hello-service.properties
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
